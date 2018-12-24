1f0623a262f14b922dce4df8d536bee037828d74
alice-fox <roza_pop@mail.ru>
2018-12-24 20:20:27 +0300

    add config test

---
+++ config/test.js
@@ -0,0 +1,30 @@
+'use strict';
+
+var os = require('os');
+var _ = require('underscore');
+
+var getExternalIP = function() {
+	return _(os.networkInterfaces()).chain().find(function(ni, name) {
+		return name !== 'lo';
+	}).find(function(ni) {
+		return !ni.internal && ni.family === 'IPv4';
+	}).value().address;
+};
+
+exports.config = {
+	env: 'test',
+	name: 'users',
+	serviceRegistry: {
+		host: '192.168.0.110',
+		port: '8500'
+	},
+	listen: {
+		host: getExternalIP(),
+		port: '8002'
+	},
+	db: {
+		type: 'mongodb',
+		url: 'mongodb://172.18.0.2:27017',
+		dbName: 'test'
+	}
+};
